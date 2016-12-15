4e5fa3931e6f491ea9568509a34ed345770156c9
OmarZV <oekhedr@yahoo.com>
2016-12-14 21:29:23 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :magazines
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, :controllers => "users_controller.rb"
+  
+  
+  root to: "books#index"
 end
